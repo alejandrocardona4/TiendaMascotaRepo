a23d0e5ef22dfbdb9b31dea22eac1a7ef5f66504
Alejandro Cardona <alejo.13cardona@gmail.com>
2025-07-19 23:05:39 -0500

    Segunda prueba

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log('Este es el mensaje de advertencia de la tienda de mascotas');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+
+// Middleware para servir archivos estáticos desde "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal (opcional, ya sirve index.html automáticamente)
+app.get('/', (req, res) => {
+  console.log('Alguien pidió la página principal');
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor levantado en http://localhost:${PORT}`);
+});
+
+
